8306e8a8764918c15dde53e6f817d7e55b434252
Marcus Bönninger <marcus.boenninger@hitabis.de>
2023-09-20 09:33:01 +0200

    GUI Form OK

--- src/web_frontend.erl
+++ src/web_frontend.erl
@@ -11,44 +11,104 @@ success() ->
 
 
 form() ->
-    << "
-<h3> Open Account </h3>
-<form method=\"post\" action=\"/accounts/open\">
-  <label for=\"accounts_firstname\"> Firstname </label>
-  <input type=\"text\" id=\"accounts_firstname\" name=\"accounts_firstname\" />
+  << "
+<h3> Request statement </h3>
+<form method=\"post\" action=\"/statements/request\">
+<table>
+     <tr>
+     <td>
+       <label for=\"statement_accountnumber\"> Account number </label>
+     </td>
+     <td>
+       <input type=\"text\" id=\"statement_accountnumber\" 
+              name=\"statement_accountnumber\" />
+     </td>
+     </tr>
 
-  <label for=\"accounts_secondname\"> Secondname </label>
-  <input type=\"text\" id=\"accounts_secondname\" name=\"accounts_secondname\" />
+     <tr>
+     <td>
+       <label for=\"statement_currency\"> Currency </label>
+     </td>
+     <td>
+       <input type=\"text\" id=\"statement_currency\" name=\"statement_currency\"
+              value=\"EUR\" size=3/>
+     </td>
+     </tr>
 
-  <input type=\"submit\" value=\"Open account\" />
+     <tr>
+     <td>
+       <label for=\"statement_format\"> Number & date formatting </label>
+     </td>
+     <td>
+       <select id=\"statement_format\" name=\"statement_format\">
+         <option value=\"de\" selected>DE</option>
+         <option value=\"en\">EN</option>
+       </select>
+     </td>
+     </tr>
+ </table>
+
+<input type=\"submit\" value=\"Request statement\" />
 </form>" >>.
 
+welcome() ->
+  << "<h1> Welcome to the Erlang Bank </h1>" >>.
+
+index() ->
+  io_lib:format("~s~s",
+                [welcome(),
+                 form()]).
 
 
-init(Req, add) ->
+back_button() ->
+  "<a href=\"/\">Back </a>".
 
-    logger:info("Creating new account"),
+-spec bin_to_int(binary()) -> integer().
+bin_to_int(B) ->
+    erlang:list_to_integer(binary:bin_to_list(B)).
 
-    {ok, KeyValuesL, _} = cowboy_req:read_urlencoded_body(Req),
+    
+% -spec statement(#account{}, list(#transfer{}), string(), number_formatter:locale()) -> string().
+statement(Account, Transfers, Currency, Format) ->
+    % TODO append all transfers
+    <<"">>.
 
-    KeyValues = maps:from_list(KeyValuesL),
-    Firstname = maps:get(<<"accounts_firstname">>, KeyValues),
-    Secondname = maps:get(<<"accounts_secondname">>, KeyValues),
 
-    % dispatch
-    AccountNumber = 1,
-    Body = io_lib:format(success(), [AccountNumber]),
+%% /statements/request
+init(Request, request_statement) ->
 
-    Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Body, Req),
+  {ok, KeyValuesL, _} = cowboy_req:read_urlencoded_body(Request),
 
-    logger:info("Created account with account number ~p", [AccountNumber]),
+  KeyValues = maps:from_list(KeyValuesL),
+  AccountNumber = bin_to_int(maps:get(<<"statement_accountnumber">>, KeyValues)),
+  Currency = binary_to_list(maps:get(<<"statement_currency">>, KeyValues)),
+  Format = list_to_atom(
+             binary_to_list(maps:get(<<"statement_format">>, KeyValues))),
 
-    {ok, Req2, []};
+  logger:info("Bank statement request: account number ~p~n", [AccountNumber]),
+  Ret = business_logic:get_account(AccountNumber),
+  KnowsCurrency = exchange_service:knows_currency(Currency),
+  case {KnowsCurrency, Ret} of
+      {true, {ok, Account}} ->
+          Transfers = business_logic:get_transfers(AccountNumber),
+          Body = statement(Account, Transfers, Currency, Format),
+          Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
+                                  Body, Request),
+          {ok, Reply, []};
+      {false, _} ->
+          Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
+                                  "Currency not found.<br/>" ++ back_button(), Request),
+          {ok, Reply, []};
+      {_, {error, not_found}} ->
+          Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
+                                  "Account not found.<br/>" ++ back_button(), Request),
+          {ok, Reply, []}
+  end;
+
 
 init(Req0, index) ->
-    ResponseBody = form(),
     Req = cowboy_req:reply(200,
                            #{<<"content-type">> => <<"text/html">>},
-                           ResponseBody,
+                           index(),
                            Req0),
     {ok, Req, []}.
