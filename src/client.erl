7330edbfaf103f0f622249a2c25754d48dd265d9
Oliver Marienfeld <oliver.marienfeld@brandad.de>
2024-06-12 16:26:08 +0200

    Fix warnings

--- src/client.erl
+++ src/client.erl
@@ -4,20 +4,24 @@
 -include("data.hrl").
 -export([open_account/4, transfer/3, statement/1]).
 
-%% returns the name of the person associated to the account 
+%% returns the name of the person associated to the account
 %% given by account number.
 -spec name_by_account_number(account_number()) -> string().
 name_by_account_number(AccountNumber) ->
     {ok, Account} = business_logic:get_account(AccountNumber),
-    {ok, Person}  = business_logic:get_person(Account#account.person_id),
+    {ok, Person} = business_logic:get_person(Account#account.person_id),
     binary_to_list(Person#person.given_name) ++ " " ++ binary_to_list(Person#person.surname).
 
 %% opens an acocunt with a given name and surname.
 %% prints the result and the account number to stdout.
--spec open_account(string(), string(), number(),number()) -> ok.
+-spec open_account(string(), string(), number(), number()) -> ok.
 open_account(GivenName, Surname, AccountNr, Amount) ->
-    Account = business_logic:open_account(list_to_binary(GivenName),
-                                          list_to_binary(Surname),AccountNr,Amount),
+    _Account = business_logic:open_account(
+        list_to_binary(GivenName),
+        list_to_binary(Surname),
+        AccountNr,
+        Amount
+    ),
     io:format("Account was successfully opened. Account number: ~p ~n", [AccountNr]).
 
 %% transfers a given amount from the first account to the second account, identified
