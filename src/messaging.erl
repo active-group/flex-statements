5165688115367901faeb3f62af31f7b032bbda6f
Oliver Marienfeld <oliver.marienfeld@brandad.de>
2024-06-13 13:19:35 +0200

    Bugfix: handle_cast statt handle_call

--- src/messaging.erl
+++ src/messaging.erl
@@ -14,7 +14,8 @@
     account_number :: account_number_dto(),
     given_name :: binary(),
     surname :: binary(),
-    amount :: number()
+    amount :: number(),
+    person_id :: number()
 }).
 
 -record(transaction_succeeded, {
@@ -30,33 +31,32 @@ init(InitialN) ->
 
 -type mutation_message() :: #account_created{} | #transaction_succeeded{}.
 
--spec handle_call(mutation_message(), gen_server:from(), receive_server_state()) ->
-    {reply, {ok, account_number_dto()}, receive_server_state()}
-    | {reply, {ok, transaction_number_dto()}, receive_server_state()}
-    | {reply, {error, term()}}.
-handle_call(
+-spec handle_cast(mutation_message(), receive_server_state()) ->
+    {noreply, receive_server_state()}.
+handle_cast(
     % gen_server:call(<0.263.0>, {account_created,3,"Peter","Lustig",2000}).
     #account_created{
         account_number = AccountNr,
         given_name = GivenName,
         surname = Surname,
-        amount = Amount
+        amount = Amount,
+        person_id = _PersonId
     },
-    _From,
     State
 ) ->
     case business_logic:open_account(GivenName, Surname, AccountNr, Amount) of
         {ok, _Account} ->
-            {reply,
-                % Antwort
-                {ok, AccountNr},
-                % neuer Zustand (keine Änderung)
-                State};
+            io:format("Konto ~w gespeichert~n", [AccountNr]),
+            notify_accounts_processed(AccountNr),
+            % neuer Zustand (keine Änderung)
+            {noreply, State};
         {error, account_exists} ->
-            {reply, {error, {account_exists, AccountNr}}, State}
+            io:format("Kontonummer ~w existiert bereits - schade~n", [AccountNr]),
+            notify_accounts_processed(AccountNr),
+            {noreply, State}
     end;
 % transfer(account_number(), account_number(), money())
-handle_call(
+handle_cast(
     % gen_server:call(<0.263.0>, {transaction_succeeded,101,1,2,1000,erlang:timestamp()}).
     #transaction_succeeded{
         transaction_id = TransactionId,
@@ -65,18 +65,40 @@ handle_call(
         amount = Amount,
         timestamp = Timestamp
     },
-    _From,
     State
 ) ->
     case business_logic:transfer(TransactionId, FromAccountNr, ToAccountNr, Amount, Timestamp) of
-        {ok, _Tid} -> {reply, {ok, TransactionId}, State};
-        {error, tid_occupied} -> {reply, {error, {tid_occupied, TransactionId}}, State};
-        {error, Cause} -> {reply, {error, Cause}, State}
+        {ok, _Tid} ->
+            io:format("Banktransfer gespeichert ~w~n", [TransactionId]),
+            notify_transfers_processed(TransactionId),
+            {noreply, State};
+        {error, tid_occupied} ->
+            io:format("Transaktionsnummer ~w leider bereits belegt~n", [TransactionId]),
+            notify_transfers_processed(TransactionId),
+            {reply, State};
+        {error, Cause} ->
+            io:format("Leider ist der Fehler ~w passiert bei Verarbeitung von Transaktionsnummer ~w~n", [Cause, TransactionId]),
+            notify_transfers_processed(TransactionId),
+            {reply, State}
     end.
 
--spec handle_cast(term(), receive_server_state()) ->
+-spec notify_transfers_processed(unique_id()) -> ok.
+notify_transfers_processed(TransactionId) ->
+    notify_processed(transfer_succeeded_ack, "TRANSFERS_HOST", TransactionId).
+
+-spec notify_accounts_processed(unique_id()) -> ok.
+notify_accounts_processed(AccountId) ->
+    notify_processed(accounts, "ACCOUNTS_HOST", AccountId).
+
+-spec notify_processed(atom(), string(), unique_id()) -> ok.
+notify_processed(Sender, EnvVar, MessageId) ->
+    ReplyAdr = node_util:node_from_env(Sender, EnvVar),
+    io:format("Sende Bestätigung an ~w~n", [ReplyAdr]),
+    gen_server:cast(ReplyAdr, {ok, <<"statements">>, MessageId}).
+
+-spec handle_call(term(), gen_server:from(), receive_server_state()) ->
     {noreply, receive_server_state()}.
-handle_cast(_, State) ->
+handle_call(_, _, State) ->
     {noreply, State}.
 
 receive_server_start() ->
