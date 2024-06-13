365dd7c3975fc47c95b3eb640c433ec51118f23f
Patrick van Issem <Patrick.vanIssem@devk.de>
2024-06-13 09:38:40 +0200

    - Register statement_server Pid

--- src/erlbank_monolithic_app.erl
+++ src/erlbank_monolithic_app.erl
@@ -19,6 +19,7 @@ start_cowboy() ->
     ]),
 
     {ok,Pid} = messaging:receive_server_start(),
+    register(statement_server, Pid),
     io:format("-----> Messaging Pid: ~w~n",[Pid]),
 
     {ok, _} = cowboy:start_clear(
