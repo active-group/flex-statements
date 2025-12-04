9497accd733dc32dc088d34513ad983157872375
Mike Sperber <sperber@deinprogramm.de>
2025-12-04 08:53:20 +0100

    Supply, accounts, transfers node.

--- src/erlbank_statements_app.erl
+++ src/erlbank_statements_app.erl
@@ -25,6 +25,12 @@ start_cowboy() ->
 start(_StartType, _StartArgs) ->
     database:init_database(),
     start_cowboy(),
+
+    AccountNode = node_util:node_from_env(accounts, "ACCOUNTS_HOST"),
+    TransferNode = node_util:node_from_env(transfers, "TRANSFERS_HOST"),
+    logger:info("Consuming accounts on: ~p~n", [AccountNode]),
+    logger:info("Consuming transfers on: ~p~n", [TransferNode]),
+
     event_sink:start(),
     erlbank_statements_sup:start_link().
 
